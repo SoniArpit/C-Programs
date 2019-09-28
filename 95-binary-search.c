f5266d0172e3e645b014bb485e6d2eb2e3203def
SoniArpit <arpitsoni362@gmail.com>
2019-09-28 22:28:07 +0530

    binary search

---
+++ 95-binary-search.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void binarySearch(int a[], int l, int h, int key)
+{
+    int mid;
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (key < a[mid])
+        {
+            h = mid - 1;
+        }
+        else if (key > a[mid])
+        {
+            l = mid + 1;
+        }
+        else if (key == a[mid])
+        {
+            printf("\n%d found at location %d\n", a[mid], mid + 1);
+            break;
+        }
+    }
+}
+int main()
+{
+    int a[20], i, n, key;
+    printf("How many values will you enters? : ");
+    scanf("%d", &n);
+    printf("Enter %d values\n", n);
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("\nEnter element to find: ");
+    scanf("%d", &key);
+
+    binarySearch(a, 0, n - 1, key);
+    return 0;
+}
